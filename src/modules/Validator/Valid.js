8fa1a6b1695c4ef7d3fe3b8095df3930fb22a9a6
Vipolian <polevit@yandex.ru>
2018-03-12 03:05:42 +0300

    switch+const

--- src/modules/Validator/Valid.js
+++ src/modules/Validator/Valid.js
@@ -1,41 +1,44 @@
 'use strict';
-
   const Validator = (asArray) => {
       let passwd = null;
       let passwdc = null;
       let errors = [];
-      let generateError = (text) => {
-        let error = document.createElement('div');
+      const generateError = (text) => {
+        const error = document.createElement('div');
         error.className = 'error';
         error.style.color = 'red';
         error.innerHTML = text;
         return error;
     };
 
-    let checkFieldsPresence = (field) => {
+    const checkFieldsPresence = (field) => {
             if (!field.value) {
                 errors.push(generateError('field is empty'));
             }
         };
 
-    let checkPasswordMatch = (pswad, paswdc) => {
+    const checkPasswordMatch = (pswad, paswdc) => {
         if (paswd.value !== paswdc.value) {
             errors.push(generateError('Password doesnt match'));
         }
     };
 
-    let checkEmailCorr = (field) => {
+    const checkEmailCorr = (field) => {
         const reg = /^[a-z0-9_-]+@[a-z0-9-]+\.([a-z]{1,6}\.)?[a-z]{2,6}$/i;
         if (!reg.test(field.value)) {
             errors.push(generateError('Wrong email'));
         }
     };
       for (let value in asArray) {
-          switch (asArray[value]) {
-              case 'login': checkFieldsPresence(value);
-              case 'email': {checkEmailCorr(value); checkFieldsPresence(value);}
-              case 'password': {checkFieldsPresence(value); passwd = value;}
-              case 'passwordconf': {checkFieldsPresence(value); passwdc = value;}
+          switch (value) {
+              case 'login': checkFieldsPresence(asArray[value]);
+                  break;
+              case 'email': checkEmailCorr(asArray[value]); checkFieldsPresence(asArray[value]);
+                  break;
+              case 'password': checkFieldsPresence(asArray[value]); passwd = value;
+                  break;
+              case 'passwordconf': checkFieldsPresence(asArray[value]); passwdc = value;
+                  break;
               default: ;
           }
       }
