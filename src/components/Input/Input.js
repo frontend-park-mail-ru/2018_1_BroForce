dbb9d71c712672d40f68e5156f500da118e3248e
arthurunique24 <arthurunique24@gmail.com>
2018-03-12 03:13:38 +0300

    Component SignIn

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -3,7 +3,7 @@
 import MainComponent from '../MainComponent/MainComponent.js';
 
 export default class Form extends MainComponent {
-    constructor(type) {
-        super('input', ['menu-input'], {type: type, placeholder: 'Email address'});
+    constructor(type, id, classes = [], placeholder) {
+        super('input', classes, {type: type, placeholder: placeholder, id: id});
     }
 }
--- src/views/SignIn/signIn.js
+++ src/views/SignIn/signIn.js
@@ -1,23 +1,24 @@
 'use strict';
 
 import MainComponent from '../../components/MainComponent/MainComponent.js';
-
-const signInForm = '<form class="menu">' +
-    '<p class="menu-input">Log in or Sign up</p> ' +
-    '<input type="email" id="signInEmailInput" class="menu-input" placeholder="Email address" required>' +
-    '<input type="password" id="signInPasswordInput" class="menu-input" placeholder="Password" required>' +
-    '<a href="" id="createAccountLink" class="menu-input">Create an account</a>' +
-    '<button class="menu-input" type="submit" id="signInSubmitBtn">Sign in</button>' +
-    '</form>';
+import Block from '../../components/Block/Block.js'
+import Input from '../../components/Input/Input.js'
+import Button from '../../components/Button/Button.js';
 
 export default class SignIn extends MainComponent {
     constructor() {
         super('form', ['menu'], {});
-        this.template = Hogan.compile(signInForm);
+        // this.template = Hogan.compile(signInForm);
     }
 
     build() {
-        this.innerHTML(this.template.render());
+        // this.innerHTML(this.template.render());
+        // document.getElementById('main').appendChild(this.render());
+        this.append((new Block('p', 'Log in or Sign up', ['menu-input'], {})).render());
+        this.append((new Input('email', 'signInEmailInput', ['menu-input'], 'Email address')).render());
+        this.append((new Input('password', 'signInPasswordInput', ['menu-input'], 'Password')).render());
+        this.append((new Block('a', 'Create an account', ['menu-input'], {href: '', id: 'createAccountLink'})).render());
+        this.append((new Button('Sign In', 'submit', ['main-input'], 'signInSubmitBtn').render()));
         document.getElementById('main').appendChild(this.render());
     }
 }
