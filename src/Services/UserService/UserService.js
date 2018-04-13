a7b66cd3e8aa735c5a1c36cf309cdb337f9e8273
arthurunique24 <arthurunique24@gmail.com>
2018-04-13 04:41:34 +0300

    JSDoc documentation

--- src/Services/UserService/UserService.js
+++ src/Services/UserService/UserService.js
@@ -2,25 +2,48 @@
 
 import Transport from '../../modules/Transport/Trasport.js';
 
+/**
+ * Module user data
+ * @module UserService
+ */
 class UserService {
+    /**
+     * @constructor
+     */
     constructor() {
         this.user = null;
     }
 
+    /**
+     * Get data from backend
+     * @return {Transport|*}
+     */
     GetData() {
         return Transport.Get('/user').then((userData) => {
             this.user = userData;
         });
     }
 
+    /**
+     * Get user data
+     * @return {*}
+     */
     GetUser() {
         return this.user;
     }
 
+    /**
+     * Check, that user in system
+     * @return {*}
+     */
     IsLogIn() {
         return !!this.user;
     }
 
+    /**
+     * Log out user
+     * @return {*}
+     */
     LogOut() {
         return Transport.Post('/logout', {}).then(() => {
             this.user = null;
