5adc1f066e9997508a62d97309e658562f1f43ee
Vipolian <polevit@yandex.ru>
2018-04-13 17:54:57 +0300

    webpack+img

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+
+module.exports = {
+    entry: {
+        main: [
+            'babel-polyfill',
+            './src/static/css/form.scss',
+            './src/index.js',
+        ],
+    },
+
+    output: {
+        filename: '[name].js',
+        path: path.join(__dirname, '..', 'src/built'),
+    },
+
+    resolve: {
+        extensions: ['.js'],
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['env'],
+                    },
+                },
+            },
+            {
+                test: /\.xml/,
+                loader: 'tp-fest-loader',
+            },
+            {
+                test: /\.scss$/,
+                use: extractSass.extract({
+                    use: [{
+                        loader: 'css-loader',
+                    }, {
+                        loader: 'sass-loader',
+                    }],
+                    // use style-loader in development
+                    fallback: 'style-loader',
+                }),
+            },
+            {
+                test: /\.(png|jpg|gid|svg)$/,
+                use: [
+                    'file-loader',
+                ],
+            },
+        ],
+    },
+    plugins: [
+        extractSass,
+    ],
+};
