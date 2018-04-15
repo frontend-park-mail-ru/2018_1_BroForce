8e47fdf480bb3883ec2f715f883005f959299f08
arthurunique24 <arthurunique24@gmail.com>
2018-04-16 00:47:46 +0300

    Hogan error

--- src/views/Game/Game.js
+++ src/views/Game/Game.js
@@ -17,7 +17,7 @@ export default class Game extends MainComponent {
         this.append(new Block('p', '0', ['game-score'], {name: 'gameScore'}).render());
 
         this.append(new MainComponent('canvas', ['game-canvas'], {}).render());
-        this.append(new MainComponent('script', [], {src: 'gameModule.js'}).render());
+        this.append(new MainComponent('script', [], {}).render());
         document.getElementById('main').appendChild(this.render());
 
         new GameLogic();
--- src/views/Leaderboard/Leaderboard.js
+++ src/views/Leaderboard/Leaderboard.js
@@ -8,6 +8,7 @@ import Pagination from '../../components/Pagination/Pagination.js';
 import Router from '../../modules/Router/Router.js';
 import Transport from '../../modules/Transport/Trasport.js';
 import UserService from '../../Services/UserService/UserService.js';
+import * as Hogan from "hogan.js";
 
 export default class Leaderboard extends MainComponent {
     constructor() {
